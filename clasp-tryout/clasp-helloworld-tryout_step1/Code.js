ee91d0e3672a920cb03129667d3b92684e857d4f
louiscklaw <louiscklaw@users.noreply.github.com>
2023-09-25 14:23:25 +0800

    update,

--- clasp-tryout/clasp-helloworld-tryout_step1/Code.js
+++ clasp-tryout/clasp-helloworld-tryout_step1/Code.js
@@ -7,7 +7,7 @@ function onOpen() {
   const ui = SpreadsheetApp.getUi();
   ui.createMenu("Automator Menu " + VER)
     .addItem("Say Hello", "sayHelloworld")
-    .addItem("send 繳費通知 (未通知 -> 已通知 2222)", "getLastRow")
+    .addItem("send 繳費通知 (未通知 -> 已通知)", "getLastRow")
     .addItem("helloWorldEmail", "helloWorldEmail")
     .addToUi();
 }
--- clasp-tryout/clasp-helloworld-tryout_step1/config.js
+++ clasp-tryout/clasp-helloworld-tryout_step1/config.js
@@ -1,4 +1,4 @@
-const VER = "1.01";
+const VER = "1.02";
 const SHEET_ID = "1RVl3rksPRzoG74WM6uDcIxXiW9uX7ppJj7iKr1hEDU0";
 
 console.log("config initialized");
