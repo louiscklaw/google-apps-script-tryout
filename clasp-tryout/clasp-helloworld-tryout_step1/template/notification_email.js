0722415a2b4bdd8e86447a4539494ad7ff80d86b
louiscklaw <louiscklaw@users.noreply.github.com>
2023-09-25 16:56:45 +0800

    update,

--- clasp-tryout/clasp-helloworld-tryout_step1/template/notification_email.js
+++ clasp-tryout/clasp-helloworld-tryout_step1/template/notification_email.js
@@ -1,6 +1,58 @@
 const email_title = (course_title) => `${course_title} 取錄及付款通知`;
 
-const email_content = (chinese_name, course_name, course_code) => {
+const email_content = (
+  chinese_name,
+  course_name,
+  course_code,
+  chinese_today_string
+) => {
+  try {
+    var output = {
+      state: "init",
+      debug: { chinese_name, course_name, course_code, chinese_today_string },
+      error: {},
+    };
+    return `
+<div>
+${chinese_name || ""} 你好，<br />
+<br />
+多謝閣下報名參加<br />
+<br />
+${course_name || ""}<br />
+${course_code || ""}<br />
+<br />
+很高興通知您，課程即將開辦，請閣下於盡快完成付款收續，以確認報名。<br />
+<br />
+<br />
+<br />
+<a href="//www.google.com" target="_blank">付款連結</a><br />
+<br />
+請保留付款截圖或證明後，貼上附件後回覆本電郵。<br />
+<br />
+<br />
+如付款有任何困難，請聯絡 9134 7967 或 <a href="">回覆</a>本電郵即可<br />
+如課程前7天前仍未付款者，本司有權將課程名額轉讓至候補名單<br />
+<br />
+最後，附上二維碼付款指引教學，以供參考。謝謝 😊<br />
+<br />
+<br />
+<br />
+樂歷課程報名處<br />
+${chinese_today_string || ""}<br />
+<br />
+<br />
+<br />
+
+</div>
+`.trim();
+  } catch (error) {
+    output = { ...output, error };
+    console.log("notification_email.js errors");
+    console.log(error);
+  }
+};
+
+const email_content1 = (chinese_name, course_name, course_code) => {
   return `
 <中文姓名> 你好，
 
