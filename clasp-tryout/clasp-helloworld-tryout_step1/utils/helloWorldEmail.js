6a799dc39d807b0721ecd3b305cd9b144da99018
louiscklaw <louiscklaw@users.noreply.github.com>
2023-09-25 16:32:39 +0800

    update,

--- clasp-tryout/clasp-helloworld-tryout_step1/utils/helloWorldEmail.js
+++ clasp-tryout/clasp-helloworld-tryout_step1/utils/helloWorldEmail.js
@@ -2,7 +2,8 @@ function helloWorldEmail() {
   try {
     var subject = "Hello from Google Apps Script";
     var body = "This is the content of the email.";
-    var htmlBody = "<h1>This is the content of the email.</h1>";
+    // var htmlBody = "<h1>This is the content of the email.</h1>";
+    var htmlBody = email_content("中文名", "chinese 中文科", "CHI001");
     var recipient = "testhelloworld04@gmail.com";
     var sender = "sender@example.com";
 
